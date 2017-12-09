e29528f5bfbe6d9f606b3aab0724ef6d0686a129
S Stewart <iamtheworst@programmer.net>
2017-12-09 12:23:13 -0600

    add a few more things to install

--- install.fish
+++ install.fish
@@ -2,7 +2,29 @@
 echo "dots install v1.0"
 echo "(c) tilda 2017"
 
+echo "Installing vimrc"
 if test -e ~/.vimrc
     mv ~/.vimrc ~/.vimrc.pre-dots
-    ln -s ~/.vimrc ./.vimrc
+    ln -s ~/.vimrc $PWD/.vimrc
+else
+    ln -s ~/.vimrc $PWD/.vimrc
 end
+echo "Installing i3 config"
+if test -d ~/.config/i3
+    mv ~/.config/i3/config ~/.config/i3/config.pre-dots
+    ln -s ~/.config/i3/config $PWD/.config/i3/config
+else
+    echo "Skipping i3, most likely not installed."
+end
+echo "Installing polybar config"
+if test -d ~/.config/polybar
+    mv ~/.config/polybar/config ~/.config/polybar/config.pre-dots
+    ln -s ~/.config/polybar/config $PWD/.config/polybar/config
+else
+    echo "Skipping polybar, most likely not installed"
+end
+echo "Installing Oh My Fish"
+curl -sSL http://oh-my.fish | fish
+echo "Installing Oh My Fish packages"
+./install_packages.fish
+echo "You should be all setup! Enjoy."
